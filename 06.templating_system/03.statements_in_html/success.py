cdaba1c5caecda2ea78d4327bfd1d755024bc72b
Avinash M <avinashmohan2001@gmail.com>
2022-07-26 19:54:01 +0530

    Update success.py

--- 06.templating_system/03.statements_in_html/success.py
+++ 06.templating_system/03.statements_in_html/success.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     app.run(debug = True)
 
 # URL: http://localhost:5000/Avinash/20/39
-# Output: 
+# Output: https://github.com/avinash3699/flask-basics/blob/main/06.templating_system/03.statements_in_html/output1.jpg
 
 # URL: http://localhost:5000/Avinash/20/69
-# Output: 
+# Output: https://github.com/avinash3699/flask-basics/blob/main/06.templating_system/03.statements_in_html/output2.jpg
