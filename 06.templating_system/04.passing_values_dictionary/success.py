b158090be05683e3b6714dcd68e344542318e346
Avinash M <avinashmohan2001@gmail.com>
2022-07-26 19:54:59 +0530

    Update success.py

--- 06.templating_system/04.passing_values_dictionary/success.py
+++ 06.templating_system/04.passing_values_dictionary/success.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     app.run(debug = True)
 
 # URL: http://localhost:5000/Avinash/20/99
-# Output: 
+# Output: https://github.com/avinash3699/flask-basics/blob/main/06.templating_system/04.passing_values_dictionary/output1.jpg
 
 # URL: http://localhost:5000/Avinash/20/9
-# Output: 
+# Output: https://github.com/avinash3699/flask-basics/blob/main/06.templating_system/04.passing_values_dictionary/output2.jpg
