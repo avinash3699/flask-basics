353e8de8f0991cb4e2f5b5390e7dc73d10450ae0
Avinash M <avinashmohan2001@gmail.com>
2022-07-26 20:28:26 +0530

    Update http_methods.py

--- 05.http_methods/http_methods.py
+++ 05.http_methods/http_methods.py
@@ -23,10 +23,10 @@ if __name__ == "__main__":
     app.run(debug = True)    
     
 # Step 1: Open form.htm in the browser. The below page opens up
-# Output: 
+# Output: https://github.com/avinash3699/flask-basics/blob/main/05.http_methods/output1.jpg
 
 # Step 2: Type something in the first input area and click the 'submit - get req'
-# Output: 
+# Output: https://github.com/avinash3699/flask-basics/blob/main/05.http_methods/output2.jpg
 
 # Step 3: Go to previous page. Type something in the second input area and click the 'submit - post req'
-# Output: 
+# Output: https://github.com/avinash3699/flask-basics/blob/main/05.http_methods/output3.jpg
